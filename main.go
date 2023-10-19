90be6da0e112eb5d45364b1b96e3d02b03c5691f
nexus806 <97809927+nexus806@users.noreply.github.com>
2023-10-19 12:33:02 +0200

    hangman fonctionnel

--- main.go
+++ main.go
@@ -3,22 +3,62 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"math/rand"
 	"os"
 )
 
 func main() {
-	var letter string
-	var word []rune
-	var underscore []rune
-	var list []string
-	var count int
-	var hangman []string
-	var index int
+	jeu()
+}
 
-	/* readfile  */
+func jeu() {
+	var life int = -1
+	var letter string
+	var underscore []rune
+	//var count int
+	var index int
+	word := readFile()
+
+	fmt.Println(string(word))
+	for i := 0; i < len(word); i++ {
+		underscore = append(underscore, '_')
+	}
+	for i := 0; i < 10; i++ {
+		fmt.Println(string(underscore) + " ")
+		fmt.Print("Enter a letter: ")
+		fmt.Scanln(&letter)
+
+		for i := 0; i < len(word); i++ {
+			if string(word[i]) == letter {
+				underscore[i] = word[i]
+				index = i
+			}
+
+		}
+
+		if string(word[index]) != letter {
+			fmt.Println(life)
+			life = life + 1
+			lign := life*7 + life
+
+			for i := lign; i < lign+7; i++ {
+				fmt.Println(readHangman()[i])
+			}
+		}
+
+		if string(word) == string(underscore) {
+			fmt.Println(string(underscore))
+			fmt.Println("You win!")
+		}
+	}
+}
+
+func readFile() []rune {
+
+	var word []rune
+	var list []string
+
 	file, err := os.Open("words.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -38,72 +78,39 @@ func main() {
 
 	randIndex := rand.Intn(len(list))
 	word = []rune(list[randIndex])
-	/* end readfile */
+	return word
 
-	randIndexWord := rand.Intn(len(word))
+}
 
-	/* read hangman  */
+func readHangman() []string {
+	var hangman []string
 
-	f, err := os.Open("hangman.txt")
-
+	file, err := os.Open("hangman.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 
-	defer f.Close()
-
-	reader := bufio.NewReader(f)
-	buf := make([]byte, 16)
-
-	for {
-		n, err := reader.Read(buf)
-
-		if err != nil {
-
-			if err != io.EOF {
-
-				log.Fatal(err)
-			}
-
-			break
-		}
-
-		hangman = append(hangman, string(buf[0:n]))
+	// Read file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		hangman = append(hangman, scanner.Text())
 	}
 
-	/* end read hangman */
+	// Check for errors
 
-	if err != nil {
+	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(word))
-	for i := 0; i < len(word); i++ {
-		underscore = append(underscore, '_')
+	return hangman
+}
+
+func draw(min int, max int) []string {
+	tab := readHangman()
+	var newtab []string
+
+	for i := min; i <= max; i++ {
+		newtab = append(newtab, tab[i])
 	}
-	for i := 0; i < 10; i++ {
-		underscore[randIndexWord] = word[randIndexWord]
-		fmt.Println(string(underscore) + " ")
-		fmt.Print("Enter a letter: ")
-		fmt.Scanln(&letter)
-
-		for i := 0; i < len(word); i++ {
-			if string(word[i]) == letter {
-				underscore[i] = word[i]
-				index = i
-
-			}
-
-		}
-
-		if string(word[index]) != letter {
-			count++
-			fmt.Println(hangman[count])
-		}
-
-		if string(word) == string(underscore) {
-			fmt.Println(string(underscore))
-			fmt.Println("You win!")
-			break
-		}
-	}
+	return newtab
 }
